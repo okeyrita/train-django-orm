b21f382a0f4f40d4a000a81700b35a30c7849546
okeyrita <koteika0909@gmail.com>
2020-10-19 18:48:45 +0300

    Revert "Add models"
    This reverts commit 1d20e110b68991543f785d7f73202d999600e796.

--- test_project/catalog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('speciality/', views.speciality_page, name='speciality'),
-    path('course/', views.course_page, name='course' ),
-    path('speciality/<int:spec>', views.details_speciality, name='speciality_details'),
-]
